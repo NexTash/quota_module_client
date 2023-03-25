4ec92c4f39345ec2065abed20324c6471abc2440
Faisal Zahoor <faisal-zahoor>
2023-03-25 10:22:49 +0500

    fixes

--- quota_module_client/api.py
+++ quota_module_client/api.py
@@ -1,12 +1,13 @@
 import frappe
+import requests
+import json
 
 @frappe.whitelist(allow_guest=True)
 def verify_license(license_key=None):
     if not license_key:
         return
-        
+
     doc = frappe.get_doc("License", "License")
-    doc.license_key = license_key
-    doc.save(ignore_permissions=True)
+    doc.db_set("license_key", license_key, notify=True, commit=True)
 
     doc.verify_license()
